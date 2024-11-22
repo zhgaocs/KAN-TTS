6a39e80b7e27aa88bb7ac9df0b0299aa3a1c8d37
zhgaocs <2049878697@qq.com>
2024-11-22 18:09:26 +0800

    Add run_inference.sh script for running model inference

---
+++ kantts/inference/scripts/run_inference.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=0
+
+INPUT_FILE="../inputs/example_sentences.txt"
+OUTPUT_DIR="../outputs/$(date +%Y%m%d_%H%M%S)"
+RESOURCE_ZIP="../models/speech_sambert-hifigan_tts_chuangirl_Sichuan_16k/resource.zip"
+VOC_CKPT="../models/speech_sambert-hifigan_tts_chuangirl_Sichuan_16k/basemodel_16k/hifigan/ckpt/checkpoint_340000.pth"
+AM_CKPT="../models/speech_sambert-hifigan_tts_chuangirl_Sichuan_16k/basemodel_16k/sambert/ckpt/checkpoint_980000.pth"
+
+mkdir -p "$OUTPUT_DIR"
+
+python ../../bin/text_to_wav.py \
+    --txt "$INPUT_FILE" \
+    --output_dir "$OUTPUT_DIR" \
+    --res_zip "$RESOURCE_ZIP" \
+    --voc_ckpt "$VOC_CKPT" \
+    --am_ckpt "$AM_CKPT"
+
+echo "Inference completed! Results are saved to $OUTPUT_DIR"
